2b38143c8590535d87a78a7f39c8ecefc95df762
Kier Roman <kierroman@gmail.com>
2025-04-13 18:52:11 -0500

    update

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,7 +1,117 @@
-import React from "react";
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router";
+import { signUp } from "../../services/authService";
+import styles from './SignUpForm.module.css'
+import { UserContext } from "../../contexts/UserContext";
 
 const SignUpForm = () => {
-  return <div>SignUpForm</div>;
+  const navigate = useNavigate()
+  const { setUser } = useContext(UserContext)
+  const [message, setMessage] = useState('')
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const { firstName, lastName, username, password, email, confirmPassword } = formData
+  const handleChange = (e) => {
+    setMessage('')
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const newUser = await signUp(formData)
+      setUser(newUser)
+      navigate('/')
+    } catch (err) {
+      setMessage(err.message)
+    }
+   
+  }
+  const isFormInvalid = () => {
+    return !(firstName && lastName && username && email && password && password === confirmPassword)
+  }
+  return (
+    <main className={styles.container}>
+      <section>
+
+          <h1>Sign Up</h1>
+          <p>{message}</p>
+        <form onSubmit={handleSubmit} className={styles.container}>
+          <div>
+
+            <input
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              value={formData.firstName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="lastName"
+              placeholder="Last Name"
+              value={formData.lastName}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={formData.username}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email Address"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirm Password"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <button disabled={isFormInvalid()}>SignUp</button>
+            <button  type='button' onClick={() => navigate('/')} >Cancel</button>
+          </div>
+        </form>
+      </section>
+    </main>
+  )
 };
 
 export default SignUpForm;
